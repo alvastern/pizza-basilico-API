7f154b95c26bdd0baf3af133dbe3c7675dcb0f8f
alvastern <alva.stern02@gmail.com>
2026-05-14 19:12:10 +0200

    Skapade databasanslutning till databasen pizza_basilico i MySQL.

---
+++ models/db.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Databasanslutning med MySQL och dotenv
+const mysql = require("mysql2");
+require("dotenv").config();
+
+// Skapande av anslutning till databasen
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// Hantering av anslutning och eventuella fel
+connection.connect((err) => {
+    if (err) {
+        console.error("Fel vid anslutning till databas" + err);
+        return;
+    }
+
+    console.log("Ansluten till databas");
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const db = require("./models/db");
 
 // Middleware
 app.use(cors());
