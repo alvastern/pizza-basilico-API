fc2720815d8ed46b44661ffd97f70d2f9366c492
alvastern <alva.stern02@gmail.com>
2026-05-14 18:42:18 +0200

    Skapade en server setup i server.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 "use strict";
 
+// Server setup med användning av express och cors
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 app.use(express.static("public"));
 
+// Bestämmer vilken router som ska användas för olika endpoints
 app.listen(port, () => {
     console.log('Server körs på port ' + port);
-})
+});
+
+module.exports = app;
